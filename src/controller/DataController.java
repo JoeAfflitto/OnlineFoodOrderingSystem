d55a179abf049dd97cf958adcb8f24755c0e0278
JoeAfflitto <49567512+JoeAfflitto@users.noreply.github.com>
2022-11-01 19:45:50 -0400

    Starting to try SQL backend

--- src/controller/DataController.java
+++ src/controller/DataController.java
@@ -3,11 +3,13 @@ package controller;
 import model.Objects.Restaurant;
 import model.TableModels.RestaurantTableModel;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class DataController {
     private ArrayList<Restaurant> theRestaurantList;
     private RestaurantTableModel tableModel;
+    private Connection connection;
 
     public DataController(){
         this.theRestaurantList = new ArrayList<>();
@@ -17,12 +19,30 @@ public class DataController {
 
     }
 
-
     public ArrayList<Restaurant> getTheRestaurantList() {
         return theRestaurantList;
     }
 
     public RestaurantTableModel getTheTableModel(){
         return this.tableModel;
+    }
+
+    private void connectServer(){
+        String connectionUrl =
+                "jdbc:sqlserver://yourserver.database.windows.net:1433;"
+                        + "database=AdventureWorks;"
+                        + "user=yourusername@yourserver;"
+                        + "password=yourpassword;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+        try (connection. DriverManager.getConnection(connectionUrl)) {
+            // Code here.
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
