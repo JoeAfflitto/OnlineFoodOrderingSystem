eb5450fd0a5a0c09a092f03b3ab8db898e27df2a
colewinters <c>
2022-11-09 20:00:16 -0500

    My design pattern changes (decorator, accordion menu)

--- test/TestHarness.java
+++ test/TestHarness.java
@@ -11,8 +11,8 @@ public class TestHarness {
     public static void main(String[] args) throws Exception {
         // Valid credential: username / password):
         // End-user:  endUser / endPassword
-        // Employee: empUser / empPassword
-        // Manager: managerUser / managerPassword
+        // Employee:  empUser / empPassword
+        // Manager:  managerUser / managerPassword
 
         LoginController lc = new LoginController();
     }
